b5cacfdb596288e27545735822a5f095cc520aec
sarvzz <77474422+sarvzz@users.noreply.github.com>
2024-11-27 16:59:26 -0800

    uploading workable files for creating the world

---
+++ helper.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+                                                                                                                                   
+def load_env():
+    _ = load_dotenv(find_dotenv())
+
+def save_world(world, filename):
+    with open(filename, 'w') as f:
+        json.dump(world, f)
+
+def load_world(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+def get_together_api_key():
+     load_env()
+     together_api_key = os.getenv("TOGETHER_API_KEY")
+     return together_api_key
